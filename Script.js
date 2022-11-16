e2beaf058b93db1ea4ddc1cb1043d4f8468f61e2
Raj Pawar <Rajpawar2944@gmail.com>
2022-11-17 00:13:50 +0530

    SMTP

--- Script.js
+++ Script.js
@@ -1,3 +1,33 @@
+//SMTP
+
+let email = "";
+let fname = "";
+let phone = "";
+let fmessage = "";
+
+document.querySelector("form.myform").addEventListener("submit", function (e) {
+  email = document.getElementById("email").value;
+  fname = document.getElementById("fname").value;
+  phone = document.getElementById("phone").value;
+  fmessage = document.getElementById("message").value;
+
+  //prevent the normal submission of the form
+  e.preventDefault();
+
+  console.log(fmessage);
+
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "username",
+    Password: "password",
+    To: "them@website.com",
+    From: email ,
+    Subject: "New Registration",
+    Body: "Name: " + fname + "<br> Email: " + email + "<br> Phone no: " + phone + "<br> Message: " + fmessage ,
+  }).then((message) => alert("Details Sent Successfully !"));
+
+});
+
 // SCROLL-EFFECT
 
 $(document).ready(function () {
@@ -6,23 +36,22 @@ $(document).ready(function () {
       dh = $(document).height(),
       wh = $(window).height();
     let scrollpercent = (scroll / (dh - wh)) * 100;
-    console.log(scrollpercent);
     $("#progressbar").css("height", scrollpercent + "%");
   });
-})
+});
 
 // SCROLL-NAVBARBG
 $(document).ready(function () {
-  var $navbar = $('nav');
+  var $navbar = $("nav");
   $(window).scroll(function () {
     var current = $(this).scrollTop();
     if (current > 0) {
-      $navbar.addClass('active-scroll');
+      $navbar.addClass("active-scroll");
     } else {
-      $navbar.removeClass('active-scroll')
+      $navbar.removeClass("active-scroll");
     }
-   })
- })
+  });
+});
 
 // PRELOADER
 
@@ -80,4 +109,24 @@ function updateTimer() {
 
 setInterval(updateTimer, 1000);
 
+//SMTP-FORM
 
+// function sendEmail() {
+//   Email.send({
+//     Host: "smtp.gmail.com",
+//     Username: "rajpawa2944@gmail.com",
+//     Password: "#raj29112002",
+//     To: "avadark2944@gmail.com",
+//     From: document.getElementById("email").value,
+//     Subject: "New Entry",
+//     Body:
+//       "Name: " +
+//       document.getElementById("name").value +
+//       "<br> Email: " +
+//       document.getElementById("email").value +
+//       "<br> Phone no: " +
+//       document.getElementById("phone").value +
+//       "<br> Message: " +
+//       document.getElementById("message").value,
+//   }).then((message) => alert("Details Send Succesfully!"));
+// }
